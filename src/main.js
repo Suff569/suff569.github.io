b61a0d7396fa4d75c62a48276c68d490b4aaf78e
Suff569 <54043166+Suff569@users.noreply.github.com>
2021-09-11 06:37:24 +0300

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import NimiqFunder from './NimiqFunder.vue'
-
-Vue.config.productionTip = false
-
-const app = new Vue({
-  render: h => h(NimiqFunder)
-}).$mount('#nimiq-funder')
-export default app
